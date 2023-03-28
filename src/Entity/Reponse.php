4b3ec52cf9151216114d2611a721c64988b783fc
KilianAlves <alveskilian@gmail.com>
2023-03-28 14:07:07 +0200

    Ajout de l'entity et du repository Reponse

---
+++ src/Entity/Reponse.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ReponseRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReponseRepository::class)]
+#[ApiResource]
+class Reponse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $texte = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $ecriteDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getEcriteDate(): ?\DateTimeInterface
+    {
+        return $this->ecriteDate;
+    }
+
+    public function setEcriteDate(\DateTimeInterface $ecriteDate): self
+    {
+        $this->ecriteDate = $ecriteDate;
+
+        return $this;
+    }
+}
