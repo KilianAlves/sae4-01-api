28feb41d7ff856230288e74243cdbb59e9f7182a
KilianAlves <alveskilian@gmail.com>
2023-04-02 18:40:37 +0200

    ajout dependance et fix Utilisateur a client

--- src/DataFixtures/ReponseFixtures.php
+++ src/DataFixtures/ReponseFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ClientFactory;
+use App\Factory\QuestionFactory;
+use App\Factory\ReponseFactory;
+use App\Factory\UtilisateurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ReponseFixtures extends Fixture
+class ReponseFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        ReponseFactory::createMany(50, function () {
+            return [
+                'question' => QuestionFactory::random(),
+                'utilisateur' => ClientFactory::random(),
+            ];
+        });
+    }
 
-        $manager->flush();
+    public function getDependencies(): array
+    {
+        return [QuestionFixtures::class];
     }
 }
