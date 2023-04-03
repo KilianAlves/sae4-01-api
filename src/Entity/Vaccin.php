c930040afdc1a59543543334c28f28be3fb88a27
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-03 17:57:21 +0200

    Configuration de l'opération Get sur Vaccin

--- src/Entity/Vaccin.php
+++ src/Entity/Vaccin.php
@@ -6,12 +6,23 @@ use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
 use App\Repository\VaccinRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: VaccinRepository::class)]
-#[ApiResource(order: ['dateRappel' => 'ASC'])]
+#[ApiResource(operations: [
+    new Get(),
+],
+    normalizationContext: ['groups' => ['get_Vac']],
+    order: ['dateRappel' => 'ASC'],
+    security: "is_granted('ROLE_VETERINAIRE')")]
 #[ApiFilter(OrderFilter::class, properties: ['dateRappel', 'libelle'], arguments: ['orderParameterName' => 'order'])]
 #[ApiFilter(SearchFilter::class, properties: ['libelle' => 'partial', 'dateRappel' => 'partial'])]
 class Vaccin
@@ -19,12 +30,15 @@ class Vaccin
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_Vac'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Groups(['get_Vac'])]
     private ?\DateTimeInterface $dateRappel = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_Vac'])]
     private ?string $libelle = null;
 
     public function getId(): ?int
