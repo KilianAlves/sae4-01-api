22125d2ea551758b48af061240ae2f150d7ec29e
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-03 18:02:34 +0200

    Configuration de l'opération Patch sur Vaccin

--- src/Entity/Vaccin.php
+++ src/Entity/Vaccin.php
@@ -9,6 +9,7 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
 use App\Repository\VaccinRepository;
 use Doctrine\DBAL\Types\Types;
@@ -23,6 +24,10 @@ use Symfony\Component\Serializer\Annotation\Groups;
     new Put(
         normalizationContext: ['groups' => ['get_Vac']],
         denormalizationContext: ['groups' => ['set_Vac']],
+    ),
+    new Patch(
+        normalizationContext: ['groups' => ['get_Me', 'get_User']],
+        denormalizationContext: ['groups' => ['set_User']],
     ),
     new Delete(),
 ],
