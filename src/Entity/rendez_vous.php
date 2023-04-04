78e120bd974cba4e61e1a4d3bf0bbaa374dfa9dd
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-04 23:34:29 +0200

    Ajout d'une nouvelle route Get pour rendez_vous

--- src/Entity/rendez_vous.php
+++ src/Entity/rendez_vous.php
@@ -3,12 +3,20 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use App\Controller\RendezVousController;
 use App\Repository\RendezVousRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RendezVousRepository::class)]
-#[ApiResource]
+#[ApiResource(operations: [
+    new Get(
+        uriTemplate: '/rendez_vous/semaine',
+        controller: RendezVousController::class,
+        name: 'app_rendezvous_semaine'
+    ),
+])]
 class rendez_vous
 {
     #[ORM\Id]
