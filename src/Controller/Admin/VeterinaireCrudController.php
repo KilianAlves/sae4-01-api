99fcebfe40283de75a7fe0f85c1f191b5a422492
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:18:23 +0200

    Personalisation de la fonction configureFields pour le CRUD de Veterinaire

--- src/Controller/Admin/VeterinaireCrudController.php
+++ src/Controller/Admin/VeterinaireCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Veterinaire;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class VeterinaireCrudController extends AbstractCrudController
 {
@@ -12,14 +15,20 @@ class VeterinaireCrudController extends AbstractCrudController
         return Veterinaire::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            TextField::new('email'),
+            TextField::new('tel'),
+            TextField::new('CP')->hideOnIndex(),
+            TextField::new('ville'),
+            TextField::new('adresse'),
+            TextField::new('complementAdresse')->hideOnIndex(),
+            TextField::new('civilite'),
+            CollectionField::new('rendezVous')->hideOnForm(),
         ];
     }
-    */
 }
