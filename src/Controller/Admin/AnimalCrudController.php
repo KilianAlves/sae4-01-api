a5d022b4bdd33c2a6736868c5b1ee53e58747765
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:13:14 +0200

    Personalisation de la fonction configureFields pour le CRUD d'Animal

--- src/Controller/Admin/AnimalCrudController.php
+++ src/Controller/Admin/AnimalCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Animal;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AnimalCrudController extends AbstractCrudController
 {
@@ -12,14 +17,20 @@ class AnimalCrudController extends AbstractCrudController
         return Animal::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            DateField::new('dateNaissance'),
+            DateField::new('dateDeces'),
+            TextField::new('robe'),
+            TextField::new('numPuce'),
+            TextField::new('commentaire'),
+            NumberField::new('poids'),
+            TextField::new('numTatouage'),
+            IntegerField::new('aDisparu'),
+            IntegerField::new('estAgressif'),
         ];
     }
-    */
 }
