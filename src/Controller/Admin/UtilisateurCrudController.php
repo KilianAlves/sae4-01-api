57a6c939b05c9db9b0916e40623ff948c38ebb16
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:17:46 +0200

    Personalisation de la fonction configureFields pour le CRUD de Utilisateur

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
@@ -12,14 +14,19 @@ class UtilisateurCrudController extends AbstractCrudController
         return Utilisateur::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            TextField::new('email'),
+            TextField::new('tel'),
+            TextField::new('CP')->hideOnIndex(),
+            TextField::new('ville'),
+            TextField::new('adresse'),
+            TextField::new('complementAdresse')->hideOnIndex(),
+            TextField::new('civilite'),
         ];
     }
-    */
 }
