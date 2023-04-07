62ea12d23cd0239c6f7242a34c887c89f3889902
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:16:24 +0200

    Personalisation de la fonction configureFields pour le CRUD de Race

--- src/Controller/Admin/RaceCrudController.php
+++ src/Controller/Admin/RaceCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Race;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RaceCrudController extends AbstractCrudController
 {
@@ -12,14 +14,11 @@ class RaceCrudController extends AbstractCrudController
         return Race::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('libelle'),
         ];
     }
-    */
 }
