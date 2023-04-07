49c035468ece9b855b3e475294386f404618dcf5
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:01:28 +0200

    Ajout du lien vers le CRUD de Client

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Animal;
+use App\Entity\Client;
+use App\Entity\Creneau;
+use App\Entity\Question;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,5 +31,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         yield MenuItem::linkToCrud('Animal', 'fas fa-paw', Animal::class);
+        yield MenuItem::linkToCrud('Client', 'fas fa-circle-user', Client::class);
     }
 }
