df7918a888a2df673d5749a627b53f9f9f5a2fc5
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 01:13:43 +0200

    Personalisation de la fonction configureFields pour le CRUD de Client

--- src/Controller/Admin/ClientCrudController.php
+++ src/Controller/Admin/ClientCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Client;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ClientCrudController extends AbstractCrudController
 {
@@ -12,14 +16,22 @@ class ClientCrudController extends AbstractCrudController
         return Client::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            NumberField::new('solde'),
+            TextField::new('commentaire'),
+            TextField::new('email'),
+            TextField::new('tel'),
+            TextField::new('CP')->hideOnIndex(),
+            TextField::new('ville'),
+            TextField::new('adresse'),
+            TextField::new('complementAdresse')->hideOnIndex(),
+            TextField::new('civilite'),
+            CollectionField::new('rendezVous')->hideOnForm(),
         ];
     }
-    */
 }
