0f7b12d50e7f4bedf61f6581fd2cb91a16fa764d
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 22:37:49 +0200

    Ajout endpoint liste des rdv Client

--- src/Controller/RendezVousController.php
+++ src/Controller/RendezVousController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\DataFixtures\RendezVous;
+use App\Entity\rendez_vous;
 use App\Entity\Veterinaire;
 use App\Repository\RendezVousRepository;
 use App\Repository\VeterianireRepository;
@@ -20,30 +22,19 @@ class RendezVousController extends AbstractController
     public function __construct(RendezVousRepository $repository, VeterianireRepository $veterianireRepository)
     {
         $this->repository = $repository;
-        $this->veterianireRepository = $veterianireRepository;
     }
 
-    public function __invoke(int $id, Request $request): JsonResponse
+    public function __invoke(Request $request): array
     {
+        $user = $this->getUser();
+        $criteria = [];
+        if (in_array('ROLE_CLIENT', $user->getRoles(), true)) {
+            $criteria["client"] = $user->getId();
+        } elseif (in_array('ROLE_VETERINAIRE', $user->getRoles(), true)) {
+            $criteria["veterinaire"] = $user->getId();
 
-        //$date = $request->query->get('date') ? DateTime::createFromFormat("Y-m-d", $request->query->get('date')) : new \DateTime('today');
-        $date = new \DateTime('today');
-        $veterinaire = $this->veterianireRepository->find($id);
-        $rdv = $this->repository->findBySemaineAndVeterinaire($date, $veterinaire);
-        $listeRDV = [];
-        for ($i = 1; $i <= 7; ++$i) {
-            $index = $date->format('d-m-Y');
-            $listeRDV[$index] = [];
-            for ($j = 8; $j <= 18; ++$j) {
-                $listeRDV[$index][$j] = $j;
-            }
-            $date->modify('+1 day');
         }
-        foreach ($rdv as $r) {
-            $index = $r->getDateRdv()->format('d-m-Y');
-            unset($listeRDV[$index][$r->getHoraire()]);
-        }
-        return new JsonResponse($listeRDV);
+        return $this->repository->findBy($criteria);
     }
 
 }
---
+++ src/Controller/RendezVousSemaineController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Veterinaire;
+use App\Repository\RendezVousRepository;
+use App\Repository\VeterianireRepository;
+use DateTime;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+#[AsController]
+class RendezVousSemaineController extends AbstractController
+{
+    public function __construct(RendezVousRepository $repository, VeterianireRepository $veterianireRepository)
+    {
+        $this->repository = $repository;
+        $this->veterianireRepository = $veterianireRepository;
+    }
+
+    public function __invoke(int $id, Request $request): JsonResponse
+    {
+
+        //$date = $request->query->get('date') ? DateTime::createFromFormat("Y-m-d", $request->query->get('date')) : new \DateTime('today');
+        $date = new \DateTime('today');
+        $veterinaire = $this->veterianireRepository->find($id);
+        $rdv = $this->repository->findBySemaineAndVeterinaire($date, $veterinaire);
+        $listeRDV = [];
+        for ($i = 1; $i <= 7; ++$i) {
+            $index = $date->format('d-m-Y');
+            $listeRDV[$index] = [];
+            for ($j = 8; $j <= 18; ++$j) {
+                $listeRDV[$index][$j] = $j;
+            }
+            $date->modify('+1 day');
+        }
+        foreach ($rdv as $r) {
+            $index = $r->getDateRdv()->format('d-m-Y');
+            unset($listeRDV[$index][$r->getHoraire()]);
+        }
+        return new JsonResponse($listeRDV);
+    }
+
+}
